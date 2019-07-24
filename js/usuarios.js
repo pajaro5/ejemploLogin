166209b8a2bae56b7fd747183569a17c907edf44
Pablo <pablorobayo@gmail.com>
2019-07-24 14:09:17 -0500

    Create usuarios.js

---
+++ js/usuarios.js
@@ -0,0 +1,15 @@
+var usuarios = [
+    new Usuario(
+        "Diablo",
+        "Huma",
+        "diablo",
+        "1234"
+        ),
+    new Usuario(
+        "Pedro",
+        "Páramo",
+        "pedro",
+        "navaja"
+    )
+];
+
